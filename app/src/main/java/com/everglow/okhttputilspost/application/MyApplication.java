41386d7cd179a4621efd0b81a415d32b27a1f634
Everglowzz <839914339@qq.com>
2017-08-14 14:15:04 +0800

    Initial commit

---
+++ app/src/main/java/com/everglow/okhttputilspost/application/MyApplication.java
@@ -0,0 +1,16 @@
+package com.everglow.okhttputilspost.application;
+import android.app.Application;
+
+/**
+ * Created by EverGlow on 2017/8/14 11:23
+ */
+
+public  class MyApplication extends Application   {
+
+    private static MyApplication app;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+            app = this;
+    }
+}
---
+++ app/src/main/java/com/everglow/okhttputilspost/log/LogLevel.java
@@ -0,0 +1,14 @@
+package com.everglow.okhttputilspost.log;
+
+public enum LogLevel {
+
+  /**
+   * Prints all logs
+   */
+  FULL,
+
+  /**
+   * No com.everglow.okhttputilspost.log will be printed
+   */
+  NONE
+}
